20f6ebf393a6e40848cd82a587f532b7c1004786
aaron <santosaaron2004@gmail.com>
2021-05-31 11:45:53 -0400

    start bot

--- src/Main.py
+++ src/Main.py
@@ -1,8 +1,16 @@
 import discord
 from discord.ext import commands, tasks
 
+from vars import INSERT_TOKEN
+
 def main():
-    print('hello world')
+    bot = commands.Bot(command_prefix='.')
+
+    @bot.command()
+    async def test(ctx):
+        await ctx.send('ah shit im back')
+    
+    bot.run(INSERT_TOKEN)
 
 if __name__ == '__main__':
     main()
