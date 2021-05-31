1282a9b110f0b527d2ca86625be4aa1772ae88c0
aaron <santosaaron2004@gmail.com>
2021-05-31 12:19:50 -0400

    start admin commands

--- src/Main.py
+++ src/Main.py
@@ -1,14 +1,28 @@
 import discord
 from discord.ext import commands, tasks
 
-from vars import INSERT_TOKEN
+from vars import INSERT_TOKEN, INSERT_ADMIN
 
 def main():
     bot = commands.Bot(command_prefix='.')
+    
+    @bot.event
+    async def on_connect():
+       print('bot connected!')
+
+    @bot.event
+    async def on_disconnect():
+        print('bot disconnected!')
 
     @bot.command()
     async def test(ctx):
         await ctx.send('ah shit im back')
+
+    @bot.command()
+    async def admin(ctx, arg):
+        if ctx.author == INSERT_ADMIN and arg == 'disconnect':
+           await bot.close()
+
     
     bot.run(INSERT_TOKEN)
 
