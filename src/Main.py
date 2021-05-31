1bf86d02548585db997dd77caacf469556be2df8
aaron <santosaaron2004@gmail.com>
2021-05-31 12:02:23 -0400

    set up logger

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,19 @@
 import discord
 from discord.ext import commands, tasks
 
+import logging
+
 from vars import INSERT_TOKEN
 
 def main():
+    #SETTING UP LOGGER
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(filename='main.log', encoding='utf-8', mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
+
+    #INIT BOT
     bot = commands.Bot(command_prefix='.')
 
     @bot.command()
