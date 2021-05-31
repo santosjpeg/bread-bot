62175f5e2794e5160fde0f9bf44992b2b45b915f
aaron <santosaaron2004@gmail.com>
2021-05-31 16:41:28 -0400

    add logger + neat figlet text

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,32 @@
+########################################################
+#                                              _       #
+# ___  ___  _   _ _ __ ___ ___    ___ ___   __| | ___  #
+#/ __|/ _ \| | | | '__/ __/ _ \  / __/ _ \ / _` |/ _ \ #
+#\__ \ (_) | |_| | | | (_|  __/ | (_| (_) | (_| |  __/ #
+#|___/\___/ \__,_|_|  \___\___|  \___\___/ \__,_|\___| #
+#                                                      #
+########################################################
+
 import discord
 from discord.ext import commands, tasks
 
+#REPLACE vars.py FILE WITH DECLARED VARS IN main()
 from vars import INSERT_TOKEN, INSERT_ADMIN
 
+#misc libraries
+import logging 
+
+import datetime
+from datetime import datetime as dt
+
 def main():
+    #SETTING UP LOGGER
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(filename='main.log',encoding='utf-8',mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
+
     bot = commands.Bot(command_prefix='.')
     
     @bot.event
